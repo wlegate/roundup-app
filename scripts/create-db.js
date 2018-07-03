e3c4ed952bd7d95f70ea5426030a8cc0e21fa1d4
William LeGate <william@legate.me>
2018-07-03 15:24:20 -0700

    Adds hardcoded db connection URI

--- scripts/create-db.js
+++ scripts/create-db.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 
 const fs = require('fs');
-const pg = require('pg');
+const { Client } = require('pg');
 
-const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL,
+const client = new Client({
+  connectionString:
+    'postgres://bgltsdrdwkchnh:065603fec8872bf895608c64a8df12a79a41aea7bd9ee520a048e981282c12c4@ec2-54-225-103-255.compute-1.amazonaws.com:5432/df4tqf14k2eia6',
   ssl: true
 });
 client.connect();
