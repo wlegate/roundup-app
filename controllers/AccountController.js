c693dacdf05f738a94772c59120ba4a333108c03
Shen-Shen <wushenshen@gmail.com>
2018-07-05 10:30:24 -0700

    Account controller

--- controllers/AccountController.js
+++ controllers/AccountController.js
@@ -2,7 +2,6 @@ const client = require('./../dbclient');
 
 const fetchAccounts = (req, res, next) => {
     let user_id = res.locals.user_id;
-    console.log('fetchAccounts: ', user_id);
     // TODO: specify which columns to return
     const query = `SELECT * FROM "Account" WHERE user_id=${user_id}`;
     client.query(query, (err, response) => {
@@ -13,4 +12,20 @@ const fetchAccounts = (req, res, next) => {
     });
 }
 
-module.exports = { fetchAccounts };
+const createAccounts = (req, res, next) => {
+    let accounts = res.locals.accounts;
+    const created = [];
+    accounts.forEach((account) => {
+        const query = 'INSERT INTO "Account" (user_id, plaid_item_id, name, official_name, type, subtype, plaid_access_token) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;';
+        const values = [res.locals.user_id, res.locals.item_id, account.name, account.official_name, account.type, account.subtype, res.locals.access_token];
+        client.query(query, values, (err, response) => {
+            if (err) console.log(err.stack)
+            else {
+                created.push(response.rows[0]);
+            }
+        });
+    });
+    next();
+}
+
+module.exports = { fetchAccounts, createAccounts };
