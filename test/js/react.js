89f42cbe184191f8399e8b8e2a3d5f0415f61d9e
Will Howard <Will@Wills-MacBook-Pro.local>
2018-07-05 13:59:44 -0700

    unit tests IP. before pull

--- test/js/react.js
+++ test/js/react.js
@@ -2,6 +2,8 @@ const expect = require("expect");
 const React = require("react");
 import App from "../../public/Components/App.jsx";
 import Enzyme from "enzyme";
+import Jest from "jest";
+import shallow from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -9,9 +11,11 @@ Enzyme.configure({ adapter: new Adapter() });
 describe("React", () => {
   describe("<App />", () => {
     let wrapper;
+    let user;
 
     before(() => {
       wrapper = Enzyme.shallow(<App />);
+      
     });
     
     it('Renders a <div> with id "app-container"', () => {
@@ -19,6 +23,9 @@ describe("React", () => {
       expect(wrapper.props().id).toEqual("app-container");
     });
 
-    xit("other test", () => {});
+    it('Should handle click on login button', () => {
+      let window = wrapper.find('Login').simulate('click', { preventDefault() {} });
+      expect(window.alert).toHaveBeenCalledWith('clicked');
+    });
   });
 });
