4a22b41eab12c9d8d9c100f1e3aecdc61eeed8b6
William LeGate <william@legate.me>
2018-07-04 19:30:05 -0700

    Moves Plaid Link logic from index.html to react components

--- public/Components/loggedIn_landing/Accounts.jsx
+++ public/Components/loggedIn_landing/Accounts.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 
-const Accounts = () => (
+const Accounts = (props) => (
   <div className="account-container" class="column">
     <h3>Active Accounts</h3>
-    <button id="link-btn">Link Account</button>
+    <button id="link-btn" onClick={props.onLink}>Link Account</button>
     <div id="account">Wells Fargo Checking</div>
     <div id="account">American Express Credit</div>
   </div>
