b7714c05150812634142c9dee5689ccef6b30dbc
William LeGate <william@legate.me>
2018-07-05 11:26:44 -0700

    Removes unused comments

--- server.js
+++ server.js
@@ -177,33 +177,6 @@ app.use('/admin', admin);
 
 // LEGACY REFERENCE
 
-// app.get('/', function(request, response, next) {
-//   response.render('index.ejs', {
-//     PLAID_PUBLIC_KEY: PLAID_PUBLIC_KEY,
-//     PLAID_ENV: PLAID_ENV
-//   });
-// });
-
-// app.post('/get_access_token', function(request, response, next) {
-//   PUBLIC_TOKEN = request.body.public_token;
-//   client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
-//     if (error != null) {
-//       var msg = 'Could not exchange public_token!';
-//       console.log(msg + '\n' + JSON.stringify(error));
-//       return response.json({
-//         error: msg
-//       });
-//     }
-//     ACCESS_TOKEN = tokenResponse.access_token;
-//     ITEM_ID = tokenResponse.item_id;
-//     console.log('Access Token: ' + ACCESS_TOKEN);
-//     console.log('Item ID: ' + ITEM_ID);
-//     response.json({
-//       error: false
-//     });
-//   });
-// });
-
 // app.get('/accounts', function(request, response, next) {
 //   // Retrieve high-level account information and account and routing numbers
 //   // for each account associated with the Item.
@@ -225,67 +198,6 @@ app.use('/admin', admin);
 //   });
 // });
 
-// app.post('/item', function(request, response, next) {
-//   // Pull the Item - this includes information about available products,
-//   // billed products, webhook information, and more.
-//   client.getItem(ACCESS_TOKEN, function(error, itemResponse) {
-//     if (error != null) {
-//       console.log(JSON.stringify(error));
-//       return response.json({
-//         error: error
-//       });
-//     }
-
-//     // Also pull information about the institution
-//     client.getInstitutionById(itemResponse.item.institution_id, function(
-//       err,
-//       instRes
-//     ) {
-//       if (err != null) {
-//         var msg = 'Unable to pull institution information from the Plaid API.';
-//         console.log(msg + '\n' + JSON.stringify(error));
-//         return response.json({
-//           error: msg
-//         });
-//       } else {
-//         response.json({
-//           item: itemResponse.item,
-//           institution: instRes.institution
-//         });
-//       }
-//     });
-//   });
-// });
-
-// app.post('/transactions', function(request, response, next) {
-//   // Pull transactions for the Item for the last 30 days
-//   var startDate = moment()
-//     .subtract(30, 'days')
-//     .format('YYYY-MM-DD');
-//   var endDate = moment().format('YYYY-MM-DD');
-//   client.getTransactions(
-//     ACCESS_TOKEN,
-//     startDate,
-//     endDate,
-//     {
-//       count: 250,
-//       offset: 0
-//     },
-//     function(error, transactionsResponse) {
-//       if (error != null) {
-//         console.log(JSON.stringify(error));
-//         return response.json({
-//           error: error
-//         });
-//       }
-//       console.log(
-//         'pulled ' + transactionsResponse.transactions.length + ' transactions'
-//       );
-//       response.json(transactionsResponse);
-//     }
-//   );
-// });
-
-var server = app.listen(APP_PORT, function () {
+var server = app.listen(APP_PORT, function() {
   console.log('plaid-walkthrough server listening on port ' + APP_PORT);
 });
